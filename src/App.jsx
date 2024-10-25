6f2b89dc3f171514ec241a8a7d4b08861a5ba5dc
Sagor Ahmed <ashrafniloy512@gmail.com>
2024-10-25 10:37:51 +0600

    added essential files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./Components/Header/Header"
+import Hero from "./Components/Hero/Hero"
+
+function App() {
+
+  return (
+    <>
+      <div className="container mx-auto">
+        <Header/>
+        <Hero/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Newsletter/NewsLetter.jsx
@@ -0,0 +1,8 @@
+
+const NewsLetter = () => {
+  return (
+    <div>NewsLetter</div>
+  )
+}
+
+export default NewsLetter
